59cb57406608f3fd47e07925f52d741470ad3be3
Oleksandr Kondratenko <sashko.kondratenko@gmail.com>
2018-03-15 20:26:17 +0200

    new class

---
+++ src/CarLogic.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+
+public class CarLogic {
+
+    public ArrayList sortCarByName(String name, Car... car) {
+
+        ArrayList<Car> list = new ArrayList<Car>();
+        for (int i = 0; i < car.length; i++) {
+            if (car[i].getName().equals(name)) {
+                list.add(car[i]);
+            }
+        } return list;
+    }
+
+    public ArrayList sortCarByModel(String model, int year, Car... car){
+        ArrayList<Car> list = new ArrayList<Car>();
+        for (int i = 0; i < car.length; i++) {
+            if (car[i].getModel().equals(model) && car[i].getYear() == year){
+                list.add(car[i]);
+            }
+        } return list;
+    }
+
+    public ArrayList sortCarByYear(int year, int price, Car... car){
+        ArrayList<Car> list = new ArrayList<Car>();
+        for (int i = 0; i < car.length; i++) {
+            if (car[i].getYear() == year && car[i].getPrice() > price){
+                list.add(car[i]);
+            }
+        } return list;
+    }
+}
+
+
