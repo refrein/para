af80f97c770d5d132e1cc0fbb164ecf917df2dbb
Boris <kovalenko.connect@gmail.com>
2018-03-15 20:38:04 +0200

    new out

--- src/Runner.java
+++ src/Runner.java
@@ -16,8 +16,11 @@ public class Runner {
         Car i = new Car(2564, "Sparrow", "IG", 1993, "purple", 25692, 1234);
         Car j = new Car(3312, "Withdraw", "Blub", 2007, "yellow", 14999, 4320);
 
-        System.out.println();
-        System.out.println();
+        CarLogic carLogic = new CarLogic();
+
+        System.out.println("Task 1:\n"+carLogic.sortCarByModel("Blub", 2007, a,b,c,d,e,f,g,h,i,j));
+        System.out.println("Task 2:\n"+carLogic.sortCarByName("Sparrow"));
+        System.out.println("Task 3:\n"+carLogic.sortCarByYear(1993, 14999, a,b,c,d,e,f,g,h,i,j));
     }
 
 }
